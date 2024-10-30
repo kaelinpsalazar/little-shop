40d940477113a643625ade750438b8506f707a21
Seth Verrill <verrill.seth@gmail.com>
2024-10-30 08:00:20 -0600

    feat: adjust method for sort to not be default

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
 
-  scope :sorted_by_price, -> { order(unit_price: :asc) }
+  def self.sorted_by_price
+    order(unit_price: :asc)
+  end
 end
