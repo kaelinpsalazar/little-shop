b46ae27f8759a9343d0f09dc22af8f1aae19e4b5
Alora Riley <alora.riley@gmail.com>
2024-10-30 17:21:03 -0400

    feat: Add actions create, update, and destroy. Add routes for these actions test: Fix test to not have .first

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   
   get '/api/v1/items', to: 'api/v1/items#index'
   post "/api/v1/items", to: "api/v1/items#create"
+  post "/api/v1/merchants", to: "api/v1/merchants#create"
   patch "/api/v1/items/:id", to: "api/v1/items#update"
+  patch "/api/v1/merchants/:id", to: "api/v1/merchants#update"
   delete "api/v1/items/:id", to: "api/v1/items#destroy"
+  delete "api/v1/merchants/:id", to: "api/v1/merchants#destroy"
+
 end
