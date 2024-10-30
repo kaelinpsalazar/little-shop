b3e927d432b003506603e6f50207a6afa9700cfe
Seth Verrill <verrill.seth@gmail.com>
2024-10-30 14:41:46 -0600

    Add #show to routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get "/api/v1/items/:id/merchant", to: 'api/v1/items_merchant#index'
   
   get '/api/v1/items', to: 'api/v1/items#index'
+  get '/api/v1/items/:id', to: 'api/v1/items#show'
   post "/api/v1/items", to: "api/v1/items#create"
   patch "/api/v1/items/:id", to: "api/v1/items#update"
   delete "api/v1/items/:id", to: "api/v1/items#destroy"
