a1a0328aac1548cf15a7835185f9513710b89ac1
Seth Verrill <verrill.seth@gmail.com>
2024-10-29 18:10:54 -0600

    feat: Added conversions to attributes

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -3,6 +3,11 @@ class ItemSerializer
 
   attributes :name, :description, :unit_price, :merchant_id
 
+  attribute :unit_price do |object|
+    object.unit_price.to_f
+  end
 
-
+  attribute :merchant_id do |object|
+    object.merchant_id.to_i
+  end
 end
