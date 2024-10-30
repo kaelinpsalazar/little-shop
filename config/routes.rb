55e658d9125bca3c84649c285f6d255ca46e837e
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-10-29 18:36:49 -0600

    definitely totaly did the items create and definitely totally didnt do merchant create last time, factory bot is here too

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  post "/api/v1/merchants", to: "api/v1/merchants#create"
+  post "/api/v1/items", to: "api/v1/items#create"
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,25 +1,34 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe "Fetch All Items" do
+describe Item do
   before(:each) do
-    @item1 = Item.new(
-      name: "Item Nemo Facere",
-      description: "Sunt eum id eius magni consequuntur delectus verit...",
-      unit_price: 42.91,
-      merchant_id: 1
-      )
+    create_list(:item, 4) 
+  end
+  
+  describe "create item" do
+    it "can create a new item" do
+      new_item_params = {
+        name: "Controller",
+        description: "Play videogames",
+        unit_price: 50.99,
+        merchant_id: Merchant.first.id 
+      }
 
-    @item2 = Item.new(
-      name: "Item Expedita Aliquam",
-      description: "Voluptate aut labore qui illum tempore eius. Corru...",
-      unit_price: 687.23,
-      merchant_id: 1
-      )
-    @item3 = Item.new(    
-      name: "Item Provident At",
-      description: "Numquam officiis reprehenderit eum ratione neque t...",
-      unit_price: 159.25,
-      merchant_id: 1
-      )
+      headers = { "CONTENT_TYPE" => "application/json" }
+
+      expect(Item.all.count).to eq(4)
+
+      post "/api/v1/items", headers: headers, params: JSON.generate(item: new_item_params)
+
+      created_item = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+
+      expect(created_item[:data][:attributes][:name]).to eq(new_item_params[:name])
+      expect(created_item[:data][:attributes][:description]).to eq(new_item_params[:description])
+      expect(created_item[:data][:attributes][:unit_price]).to eq(new_item_params[:unit_price])
+      expect(created_item[:data][:attributes][:merchant_id]).to eq(new_item_params[:merchant_id])
+
+      expect(Item.all.count).to eq(5)
+    end
   end
 end
