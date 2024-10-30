af9651163096261a2248ffd6fda24ef7df3b250b
Alora Riley <alora.riley@gmail.com>
2024-10-29 23:42:11 -0400

    teat: Add test for update an existing merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -19,4 +19,21 @@ RSpec.describe "Mechant", type: :request do
     end
   end
 
+  describe "edits a merchant with #update" do
+    it "can update an existing merchant" do
+      id = Merchant.create(name: "Gnome Depot").id
+
+      previous_name = Merchant.last.name
+      merchant_params = { name: "Troll's"}
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/merchants/#{id}", headers: headers, params: JSON.generate({merchant: merchant_params})
+
+      merchant = Merchant.find_by(id: id)
+
+      expect(response).to be_successful
+      expect(merchant.name).to_not eq(previous_name)
+      expect(merchant.name).to eq("Troll's")
+    end
+  end
 end
