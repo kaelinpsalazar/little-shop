8e7b76182df7e30cb473952d2590a7fc59eaa8fb
Joel Davalos <jsdavalos@gmail.com>
2024-10-31 09:55:48 -0600

    Re organized routes in route file.

--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,24 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  
   # Defines the root path route ("/")
   # root "posts#index"
+
   get "/api/v1/merchants", to: 'api/v1/merchants#index'
   get "/api/v1/merchants/:id", to: 'api/v1/merchants#show'
+  post "/api/v1/merchants", to: "api/v1/merchants#create"
+  patch "/api/v1/merchants/:id", to: "api/v1/merchants#update"
+  delete "api/v1/merchants/:id", to: "api/v1/merchants#destroy"
+  
   get "/api/v1/items/:id/merchant", to: 'api/v1/items_merchant#index'
   get "/api/v1/merchants/:merchant_id/customers", to: 'api/v1/merchants_customers#index'
   
   get '/api/v1/items', to: 'api/v1/items#index'
   get '/api/v1/items/:id', to: 'api/v1/items#show'
   post "/api/v1/items", to: "api/v1/items#create"
-  post "/api/v1/merchants", to: "api/v1/merchants#create"
   patch "/api/v1/items/:id", to: "api/v1/items#update"
-  patch "/api/v1/merchants/:id", to: "api/v1/merchants#update"
   delete "api/v1/items/:id", to: "api/v1/items#destroy"
 
-  delete "api/v1/merchants/:id", to: "api/v1/merchants#destroy"
 
 end
 
