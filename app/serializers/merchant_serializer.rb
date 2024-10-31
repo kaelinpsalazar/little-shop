27e50238c5ac29246750766c12d5e6c28de26610
Joel Davalos <jsdavalos@gmail.com>
2024-10-31 10:17:42 -0600

    Added conditional logic for item_count into merchant serializer.

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -2,5 +2,9 @@ class MerchantSerializer
   include JSONAPI::Serializer
 
   attributes :name
+
+  attribute :item_count do |merchant, params|
+    paprams[:include_item_count] ? merchant.item_count : nil
+  end
   
 end
