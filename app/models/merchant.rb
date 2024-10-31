9dbdbaf32edeca81bdf4df8177b0c4f6632467c5
Joel Davalos <jsdavalos@gmail.com>
2024-10-31 10:05:23 -0600

    Added methods in merchant model file.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,20 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
   has_many :customers, through: :invoices
+
+  def self.sorted_by_created_at
+    order(created_at: :desc)
+  end
+
+  def self.with_returned_status
+    joins(:invoices).where(invoices: { status: 'returend'}).distinct
+  end
+
+  def self.with_item_count
+    merchants = Merchant.includes(:items)
+    merchants.map do |merchant|
+      merchant.attributes.merge('item_count => merchant.itmes.size')
+    end
+
+  end
 end
