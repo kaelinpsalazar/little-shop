1ea0308d787841d4e141944659bdb4cc772d8c73
Joel Davalos <jsdavalos@gmail.com>
2024-10-31 08:40:42 -0600

    Added route for merchant index action.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   
-  get "/api/v1/merchants/:id", to: 'api/v1/merchants#show'
   # Defines the root path route ("/")
   # root "posts#index"
-
+  get "/api/v1/merchants", to: 'api/v1/merchants#index'
+  get "/api/v1/merchants/:id", to: 'api/v1/merchants#show'
   get "/api/v1/items/:id/merchant", to: 'api/v1/items_merchant#index'
   get "/api/v1/merchants/:merchant_id/customers", to: 'api/v1/merchants_customers#index'
   
