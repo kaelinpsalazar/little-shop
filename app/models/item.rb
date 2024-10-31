7049be85011f99f6bf33c40a8a286794992a463f
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-10-31 11:52:40 -0600

    I had to update the model validation so that it didnt allow negative values for unit_price

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   has_many :invoice_items
 
   validates :name, presence: true
-  validates :unit_price, presence: true
+  validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :merchant_id, presence: true
   validates :description, presence: true
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Item, type: :model do
     it { should belong_to(:merchant) }
     it {should have_many(:invoice_items) }
     it {should validate_presence_of(:name)}
-    it {should validate_presence_of(:unit_price)}
+    it { should validate_numericality_of(:unit_price).is_greater_than_or_equal_to(0) }   
     it {should validate_presence_of(:merchant_id)}
     it {should validate_presence_of(:description)}
   end
