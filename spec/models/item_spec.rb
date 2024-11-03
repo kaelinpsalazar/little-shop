a5eb1f9c751c7e6485c0ba12e551b58c7f47618e
Alora Riley <alora.riley@gmail.com>
2024-11-03 18:05:03 -0500

    test: Add before(:each)

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  before(:each) do
+    @merchant = create(:merchant)
+    @item1 = create(:item, name: 'airbuds', unit_price: 75.00, merchant: @merchant)
+    @item2 = create(:item, name: 'hydroflask', unit_price: 20.00, merchant: @merchant)
+    @item3 = create(:item, name: 'airpump', unit_price: 55.00, merchant: @merchant)
+  end
   describe "associations" do
-    it { should belong_to(:merchant) }
+    it {should belong_to(:merchant) }
     it {should have_many(:invoice_items) }
     it {should validate_presence_of(:name)}
-    it { should validate_numericality_of(:unit_price).is_greater_than_or_equal_to(0) }   
+    it {should validate_numericality_of(:unit_price).is_greater_than_or_equal_to(0) }   
     it {should validate_presence_of(:merchant_id)}
     it {should validate_presence_of(:description)}
   end
@@ -13,12 +19,8 @@ RSpec.describe Item, type: :model do
   
   describe ".sorted_by_price" do
     it "returns items sorted by price lowest to highest" do
-      merchant = create(:merchant)
-      item1 = create(:item, unit_price: 75.00, merchant: merchant)
-      item2 = create(:item, unit_price: 20.00, merchant: merchant)
-      item3 = create(:item, unit_price: 55.00, merchant: merchant)
 
-      expect(Item.sorted_by_price).to eq([item2, item3, item1])
+      expect(Item.sorted_by_price).to eq([@item2, @item3, @item1])
     end
   end
 
