29c032600e43cbb7c6721e518bdf459ce0270ba5
Joel Davalos <jsdavalos@gmail.com>
2024-11-03 17:46:59 -0700

    Created invoices serializer and merhcant_invoices controller

---
+++ app/serializers/invoices_serializer.rb
@@ -0,0 +1,17 @@
+class InvoiceSerializer
+  include JSONAPI::Serializer
+
+  attributes :customer_id, :merchant_id, :status, :created_at, :updated_at
+
+  attribute :customer_id do |object|
+    object.customer_id.to_i
+  end
+
+  attribute :merchant_id do |object|
+    object.merchant_id.to_i
+  end
+
+  attribute :status do |object|
+    object.status.to_s
+  end
+end
