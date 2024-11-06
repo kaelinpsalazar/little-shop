d9c0cd35363c8b53ca3a79dd14002f5c02f0e26d
sethverrill <verrill.seth@gmail.com>
2024-11-06 11:12:41 -0700

    Add some 404 error handling and add tests to verify functionality

--- app/controllers/api/v1/merchants_invoices_controller.rb
+++ app/controllers/api/v1/merchants_invoices_controller.rb
@@ -1,22 +1,27 @@
 class Api::V1::MerchantsInvoicesController < ApplicationController
- def index
-  # binding.pry
-  merchant_id = params[:merchant_id]
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
-  if params[:status].present?
-    status = params[:status]
+  def index
+    merchant_id = params[:merchant_id]
 
-    if Invoice.valid_status?(status)
-      invoices = Invoice.for_merchant_with_status(merchant_id, status)
-      render json: InvoiceSerializer.new(invoices), status: :ok
+    if params[:status].present?
+      status = params[:status]
+
+      if Invoice.valid_status?(status)
+        invoices = Invoice.for_merchant_with_status(merchant_id, status)
+        render json: InvoiceSerializer.new(invoices), status: :ok
+      else
+        render json: { error: 'Invalid status' }, status: :bad_request
+      end
     else
-      render json: { error: 'Invalid status' }, status: :bad_request
-    end
-  else
-    merchant = Merchant.find(merchant_id)
-    render json: InvoiceSerializer.new(merchant.invoices), status: :ok
+      merchant = Merchant.find(merchant_id)
+      render json: InvoiceSerializer.new(merchant.invoices), status: :ok
+    end  
   end
-  
-  
-end
+
+  private
+
+  def not_found_response(exception)
+    render json: ErrorSerializer.format_error(exception, 404), status: :not_found
+  end
 end
